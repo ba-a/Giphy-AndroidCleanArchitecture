17de7798ac8e137e8c41d36bb77c3cdb28d09d3d
Andreas Bauer <abauer@inovex.de>
2020-05-27 22:31:41 +0200

    Rename Api Error Handler

---
+++ app/src/main/java/de/giphy_clean_architecture/data/service/ApiErrorHandler.kt
@@ -0,0 +1,29 @@
+package de.giphy_clean_architecture.data.service
+
+import de.giphy_clean_architecture.domain.model.ErrorModel
+import retrofit2.HttpException
+import timber.log.Timber
+
+class ApiErrorHandler {
+    private val TAG = "ApiErrorHandle"
+
+    fun traceErrorException(
+        throwable: Throwable?
+    ): ErrorModel? {
+        Timber.tag(TAG).e(throwable)
+        return when (throwable) {
+            is HttpException -> {
+                ErrorModel(
+                    null,
+                    throwable.code(),
+                    errorType = ErrorModel.ErrorType.FULL_SCREEN
+                )
+            }
+            else -> ErrorModel(
+                null,
+                0,
+                errorType = ErrorModel.ErrorType.FULL_SCREEN
+            )
+        }
+    }
+}
